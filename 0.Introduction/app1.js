cac96304c199810c05800dc1e45f3917369d89ea
MercyXp <munzenzimercy2024@gmail.com>
2024-11-21 14:34:04 +0700

    app-1 updated

---
+++ 0.Introduction/app1.js
@@ -0,0 +1,29 @@
+//printing hello world
+
+console.log("Hello World")
+//statements
+//Semicolons separate JavaScript statements
+a = 23;
+b = 76;
+z = a + b;
+document.write(z);
+console.log(z);
+
+document.write("  The 'document.write()' is used when we want to print the content onto the document which is the HTML Document.");
+
+alert("hey"); //the alert() function is not available in the Node.js environment. The alert() function is part of the browser's built-in APIs (such as the window object) and is used for displaying popup dialogs in web browsers. It is not supported in server-side environments like Node.js.
+
+//This is a single line comment
+
+/*This is 
+a multiline
+comment
+*/
+
+//statements
+//Semicolons separate JavaScript statements
+a = 23;
+b = 76;
+z = a + b;
+document.write(z);
+console.log(z);
